42f84930020ddbd3e2e1e7d8476073322279eede
pedrro18 <phenriqueafelix18@gmail.com>
2024-11-07 10:31:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Esporte',
+    'Ultimo brasileiro compeão do mundial foi?',
+    'Foi o Corinthians em 2012'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Esporte',
+    'Maior campeão Brasileiro?',
+    'O maior campeão Brasileiro é o Palmeiras,com 12 titulos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'Eu te amo em ingles seria I love you'
+)
